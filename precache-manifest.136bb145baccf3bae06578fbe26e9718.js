6318ade0d574a879a85be47a651cbbdc46418fa7
Vinicius Silva <viniciussilvavpp@gmail.com>
2018-12-14 12:39:45 -0200

    React + Google Spreadsheets

---
+++ precache-manifest.136bb145baccf3bae06578fbe26e9718.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5f3b3b3fde459e9faf51",
+    "url": "/static/js/main.5f3b3b3f.chunk.js"
+  },
+  {
+    "revision": "b9c076adb7c2aa1dbf3f",
+    "url": "/static/js/1.b9c076ad.chunk.js"
+  },
+  {
+    "revision": "5f3b3b3fde459e9faf51",
+    "url": "/static/css/main.14783835.chunk.css"
+  },
+  {
+    "revision": "b9c076adb7c2aa1dbf3f",
+    "url": "/static/css/1.5920067b.chunk.css"
+  },
+  {
+    "revision": "35e48b9ee44a68c8c2ea962ec9c9777c",
+    "url": "/index.html"
+  }
+];
